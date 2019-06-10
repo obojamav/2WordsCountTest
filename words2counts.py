941aca1eed9e50b5fd39c80b8ee93713a821377d
obojamav <38743915+obojamav@users.noreply.github.com>
2019-06-11 00:48:17 +0700

    Update words2counts.py

--- words2counts.py
+++ words2counts.py
@@ -3,10 +3,26 @@
 from pyspark import SparkContext
 import collections
 
+def mapper(all_data):
+    map_list = []
+
+    for line in all_data:
+
+        w1 = line.split()
+        w2 = collections.deque(words)
+        w2.rotate(1)
+
+        w1 = list(map(lambda a, b: a+ " " +b,list(w2), w1))
+        del w1[0]
+
+        map_list.append(w1)
+
+    return map_list
+
 def main():
     sc = SparkContext(appName="2WordCount")
     input_file = sc.textFile('/user/cloudera/wz/test_wordcount.txt')
-    token = input_file.flatMap(lambda line:line.split())
+    token = input_file.flatMap(mapper)
     words = token.map(lambda word : (word, 1))
     wz = words.reduceByKey(lambda a, b: a + b)
     wz.saveAsTextFile('/user/cloudera/wz/output')
