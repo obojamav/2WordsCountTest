a517f9470884e9acb85105ea422668ea663d0c7d
obojamav <38743915+obojamav@users.noreply.github.com>
2019-06-11 00:36:04 +0700

    Update words2counts.py

--- words2counts.py
+++ words2counts.py
@@ -7,7 +7,7 @@ def main():
     sc = SparkContext(appName="2WordCount")
     input_file = sc.textFile('/user/cloudera/wz/test_wordcount.txt')
     token = input_file.flatMap(lambda line:line.split())
-    words = token.map(lambda word : ((word, 1)))
+    words = token.map(lambda word : (word, 1))
     wz = words.reduceByKey(lambda a, b: a + b)
     wz.saveAsTextFile('/user/cloudera/wz/output')
     sc.stop()
